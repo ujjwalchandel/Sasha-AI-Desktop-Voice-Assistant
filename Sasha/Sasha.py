59f7c4c2bd5b0010db4825e9d5ecf6374d0b6db8
ujjwalchandel <164720663+ujjwalchandel@users.noreply.github.com>
2024-04-16 22:01:32 +0530

    Add files via upload

---
+++ Sasha/Sasha.py
@@ -0,0 +1,18 @@
+
+import pyttsx3
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    speak("hello boss, i am sasha. how can i help you?")
+
+
+

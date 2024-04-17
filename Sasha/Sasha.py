d9faeefaf02e5b54e1d23d3aa44276720196297b
ujjwalchandel <ujjwalraj8935@gmail.com>
2024-04-17 12:18:55 +0530

    wikipedia imported and run successfully

--- Sasha/Sasha.py
+++ Sasha/Sasha.py
@@ -1,9 +1,11 @@
-
 import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-print(voices[1].id)
+# print(voices[1].id)
 engine.setProperty('voice', voices[1].id)
 
 
@@ -11,8 +13,48 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("Hello sir, i'm Sasha. How can i help you? ")
+
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)
+
+        print("Say that again please...")
+        return "None"
+    return query
+
 if __name__ == "__main__":
-    speak("hello boss, i am sasha. how can i help you?")
+    wishMe()
+    while True:
+        query = takeCommand().lower()
 
-
+        if 'wikipedia' in query:
+            speak("Searching wikipedia...")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            speak(results)
 

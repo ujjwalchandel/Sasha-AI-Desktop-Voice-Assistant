a4d88ab84bc19b5623db5e8230659ce01372cd54
ujjwalchandel <ujjwalraj8935@gmail.com>
2024-04-27 23:29:23 +0530

    camera, news, intro features added

--- Sasha/Sasha.py
+++ Sasha/Sasha.py
@@ -2,6 +2,13 @@ import pyttsx3
 import speech_recognition as sr
 import datetime
 import wikipedia
+import webbrowser
+import os
+import smtplib
+import ecapture
+import json
+from urllib.request import urlopen
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -61,8 +68,6 @@ if __name__ == "__main__":
     if 1:
         query = takeCommand().lower()
 
-        # logic for executing tasks based on query
-
         if 'wikipedia' in query:
             speak("Searching wikipedia...")
             query = query.replace("wikipedia", "")
@@ -71,3 +76,70 @@ if __name__ == "__main__":
             print(results)
             speak(results)
 
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open facebook' in query:
+            webbrowser.open("facebook.com")
+
+        elif 'open instagram' in query:
+            webbrowser.open("instagram.com")
+
+        elif 'paly music' in query:
+            music_dir = 'D:\\ Non Criticals\\...'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath= "C:\\Users\\ujjwa\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'open virtual studio' in query:
+            codePath= "C:\\Users\\ujjwa\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif "camera" in query or "take a photo" in query:
+            ecapture.capture(0, "Jarvis Camera ", "img.jpg")
+
+        elif "who are you" in query:
+            speak("I am your advanced virtual assistant created by Team Tera Baap")
+
+        elif 'news' in query:
+             
+            try: 
+                jsonObj = urlopen('''https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=top&apiKey=4c6fd3c9a0754c898d9c70d3d2e41a7b''')
+                data = json.load(jsonObj)
+                i = 1
+                 
+                speak('here are some top news from the times of india')
+                print('''=============== TIMES OF INDIA ============'''+ '\n')
+                 
+                for item in data['articles']:
+                     
+                    print(str(i) + '. ' + item['title'] + '\n')
+                    print(item['description'] + '\n')
+                    speak(str(i) + '. ' + item['title'] + '\n')
+                    i += 1
+            except Exception as e:
+                 
+                print(str(e))
+
+
+        # elif 'send email to ujjwal' in query:
+        #     try:
+        #         speak("what should i send ?")
+        #         content= takeCommand()
+        #         to = "ujjwalraj8935@gmail.com"
+        #         sendEmail(to, content)
+        #         speak("email has been sent!")
+        #     expect exception as e:
+        #         print(e)
+        #     speak("sorry sir, i'm not able to send this email")
